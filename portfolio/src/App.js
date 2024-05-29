07619f183b2129c7a5037e701444bbec82635270
Thrishal <thrishalmadasu@gmail.com>
2024-05-29 13:07:19 +0530

    added home page

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,20 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Lottie from  "lottie-react";
-import nightsky from "./LottieFiles/night-sky.json";
-import Projects from './components/Projects';
 import Navbar from './components/Navbar';
-import Typed from './components/Typed';
+import Home from './components/Home';
 
 
 function App() {
   return (
-    <div>  
+    <div>
       <Navbar />
-      <h1>
-        Hey! I am Thrishal
-      </h1>
-      <Typed />
+      <Home />
     </div>
   );
 }
