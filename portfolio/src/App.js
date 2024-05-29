fe47f659ba85157e902ad936228314256fa3e7f6
Thrishal <thrishalmadasu@gmail.com>
2024-05-29 12:48:36 +0530

    Added Type Writer Effect

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -4,12 +4,17 @@ import Lottie from  "lottie-react";
 import nightsky from "./LottieFiles/night-sky.json";
 import Projects from './components/Projects';
 import Navbar from './components/Navbar';
+import Typed from './components/Typed';
 
 
 function App() {
   return (
     <div>  
       <Navbar />
+      <h1>
+        Hey! I am Thrishal
+      </h1>
+      <Typed />
     </div>
   );
 }
